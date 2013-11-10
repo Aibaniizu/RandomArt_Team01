e819430b564165a3dcb34bd8c55d4feed1db524f
ernggkiddie <erng.me@gmail.com>
2013-11-10 23:15:27 +0700

    Try to do

--- RandomArt/src/randomart/Exp.java
+++ RandomArt/src/randomart/Exp.java
@@ -5,11 +5,18 @@
  */
 
 package randomart;
-
+import javax.script.*;
 /**
  *
  * @author Chanidapa
  */
 public class Exp {
-    
+    private Eval obj;
+    private Eval eval;
+    ScriptEngineManager factory = new ScriptEngineManager();
+    // create a JavaScript engine
+    ScriptEngine engine = factory.getEngineByName("JavaScript");
+    // evaluate JavaScript code from String
+    //Eval obj = engine.eval("1+2");
+    //System.out.println( obj );
 }
