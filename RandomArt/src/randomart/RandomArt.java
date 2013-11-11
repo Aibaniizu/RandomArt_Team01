fef4b3f7b20c9f04288a63ec22158bebe267a6ef
Aibaniizu <tiialle@windowslive.com>
2013-11-11 11:45:47 +0700

    Changes to our main class

--- RandomArt/src/randomart/RandomArt.java
+++ RandomArt/src/randomart/RandomArt.java
@@ -1,28 +1,49 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 
 package randomart;
-
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Random;
 import javax.swing.JFrame;
+
 
 /**
  *
- * @author Tiia and Erngg
+ * @author Tiia
  */
-public class RandomArt {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        GUI gui = new GUI(200,200,1);
-        
-        
-        gui.applyColor();
+public class RandomArt extends Canvas {
+    private static final int WIDTH = 200;
+    private static final int HEIGHT = 200;
+    private static final Random random = new Random();
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+
+        for(int x = 0; x < WIDTH; x++) {
+            for(int y = 0; y < HEIGHT; y++) {
+                g.setColor(randomColor());
+                g.drawLine(x, y, x, y);
+                
+            }
+        }
     }
-    
+
+    private Color randomColor() {
+        return new Color(random.nextInt(256), random.nextInt(256), random.nextInt(256));
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+
+        frame.setSize(WIDTH, HEIGHT);
+        frame.add(new RandomArt());
+
+        frame.setVisible(true);
+    }
 }
