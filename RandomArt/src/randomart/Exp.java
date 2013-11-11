c2733406b7aecabdde110d340ace777266eff651
ernggkiddie <erng.me@gmail.com>
2013-11-11 23:36:13 +0700

    add more method

--- RandomArt/src/randomart/Exp.java
+++ RandomArt/src/randomart/Exp.java
@@ -5,12 +5,47 @@
  */
 
 package randomart;
-import javax.script.*;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptEngine;
+import java.util.Random;
+
 /**
  *
- * @author Chanidapa
+ * @author Chanidapa 55130500210
  */
 public class Exp {
+
+    
+    
+    //random x,y
+    public void xy(){
+        
+    }
+    
+    //random function
+    public void randomFunc(){
+        String[] func;
+        //func = {"sin", "cos", "avg"};
+        Random function = new Random();
+        //int select = function.nextInt(func.length);
+    }
+    
+    //check case
+    public void checkCase(){
+        /*switch(select){
+            case "sin": System.out.println("sin");
+                break;
+            case "cos": System.out.println("cos");
+                break;
+            case "avg"
+        }*/
+    }
+    
+    public void avg(){
+        
+    }
+    
+
     private Eval obj;
     private Eval eval;
     ScriptEngineManager factory = new ScriptEngineManager();
