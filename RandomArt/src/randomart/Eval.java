ce972e44b0ca4ee89b5d83a487046d4432928f61
ernggkiddie <erng.me@gmail.com>
2013-11-12 22:58:50 +0700

    create x,y, and z

--- RandomArt/src/randomart/Eval.java
+++ RandomArt/src/randomart/Eval.java
@@ -11,5 +11,15 @@ package randomart;
  * @author Chanidapa
  */
 public class Eval {
-    
+     private int x;
+     private int y;
+     private int z;
+
+    public Eval() {
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
+    }
+     
+     
 }
