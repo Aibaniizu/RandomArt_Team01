3130f61f00f8284348e49b6f867ae0da2c1e3b84
ernggkiddie <erng.me@gmail.com>
2013-11-12 23:11:48 +0700

    create constructor Eval that have parameter (Exp exp, int x, int y)

--- RandomArt/src/randomart/Eval.java
+++ RandomArt/src/randomart/Eval.java
@@ -17,14 +17,18 @@ public class Eval {
      private Exp exp;
 
     public Eval() {
+        this.x = 0;
+        this.y = 0;
         this.z = 0;
     }
     
+     private Eval(Exp exp, int x, int y) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+     
     public void randomXY(){
         this.x = (int) Math.random();
         this.y = (int) Math.random();
         //this.z = Eval(exp, x, y);
-    }
-     
-     
+    }  
 }
