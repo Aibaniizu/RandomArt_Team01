4278b5b0eb5689ec9cd7c6c0381bdacfdd8a8b80
ernggkiddie <erng.me@gmail.com>
2013-11-15 23:29:43 +0700

    remove evaluate from expression class

--- RandomArt/src/randomart/Exp.java
+++ RandomArt/src/randomart/Exp.java
@@ -14,7 +14,7 @@ import java.util.Random;
  * @author Chanidapa 55130500210
  */
 public class Exp {
-    private int count;
+    /*private int count;
 
     public Exp() {
         this.count = 0;
@@ -33,43 +33,32 @@ public class Exp {
         Random function = new Random();
         int select = function.nextInt(func.length);
         return func;
-    }
+    }*/
     
     //check case
-    public void checkCase(){
+    //public void checkCase(){
         /*switch(select){
             case "sin": System.out.println("sin");
                 break;
             case "cos": System.out.println("cos");
                 break;
             case "avg"
-        }*/
-    }
+        }
+    }*/
     
     //average
-    public void avg(){
+    /*public void avg(){
         String[] temp;
         temp = randomFunc();
         char braces;
         braces = (char) checkBraces();
         System.out.println(temp + "(pi*" + braces);
-    }
+    }*/
     
     //count braces
-    public int checkBraces(){
+    /*public int checkBraces(){
         String i = "(";
         count++;
         return count;
-    }
-
-    private Eval obj;
-    private Eval eval;
-    ScriptEngineManager factory = new ScriptEngineManager();
-    // create a JavaScript engine
-    ScriptEngine engine = factory.getEngineByName("JavaScript");
-    // evaluate JavaScript code from String
-    //Eval obj = engine.eval("1+2");
-    //System.out.println( obj );
-    
-    //should buildExp(); method be on this class?
+    }*/
 }
