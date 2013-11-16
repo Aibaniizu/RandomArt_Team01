7f8e1c888ce82c748cda2796fcb9e4c814fc7377
ernggkiddie <erng.me@gmail.com>
2013-11-17 00:50:01 +0700

    new version of expression

--- RandomArt/src/randomart/Exp.java
+++ RandomArt/src/randomart/Exp.java
@@ -4,7 +4,15 @@
      * and open the template in the editor.
      */
 
-    package randomart;
+ package randomart;
+
+
+
+
+import java.util.LinkedList;
+
+
+
 
     /**
      *
@@ -17,15 +25,38 @@
         private int count = 0;
         private int level = 5;
         private String expressionString = "";
+        private LinkedList<Integer> stage;
         
-         public Exp(double x, double y){
+         public Exp(){
+             this.stage = new LinkedList<>();
+         }
+        
+         public void setXY(double x, double y){
              this.x = x;
              this.y = y;
-             
+         }
+         public String getExpString(){
+             int countOpen = 0;
+             int countClose = 0;             
+             for(int i = 0; i < expressionString.length(); i++){
+                 if(expressionString.charAt(i) == '(' ){
+                     countOpen++;
+                 }else if(expressionString.charAt(i) == ')'){
+                     countClose++;
+                 }         
+             }             
+             for(int i = 0; i < countOpen - countClose; i++){
+                 expressionString += ")";
+             }             
+             return expressionString;
          }
          
-         public double evaluate(boolean closeBrack){
-             int random;
+         public LinkedList getStage(){
+             return this.stage;
+         }
+               
+         public double genExp(boolean closeBrack){
+              int random;
               if(this.count == 0){
                   random = (int) (Math.random()*2);
                   this.count++;
@@ -40,21 +71,24 @@
                switch(random){
                    case 0: 
                        expressionString += "sin(pi*";
-                       return Math.sin(Math.PI*evaluate(true));
+                       this.stage.add(0);
+                       return Math.sin(Math.PI*genExp(true));
                        
                    case 1:
                        expressionString += "cos(pi*";
-                       return Math.cos(Math.PI*evaluate(true));
+                       this.stage.add(1);
+                       return Math.cos(Math.PI*genExp(true));
                        
                    case 2:
                        expressionString += "avg(";
-   
-                       double first = evaluate(false);
+                       this.stage.add(2);
+                       double first = genExp(false);
                        expressionString += ",";
-                       double second = evaluate(true);
+                       double second = genExp(true);
                        expressionString += ")";
                        return avg(first, second);
                    case 3: 
+                       this.stage.add(3);
                        if(closeBrack == true){
                            expressionString += "x)";
                        }else{
@@ -64,6 +98,7 @@
                        return x;
                        
                    default:
+                       this.stage.add(4);
                        if(closeBrack == true){
                            expressionString += "y)";
                        }else{
@@ -78,34 +113,64 @@
              return (x+y)/2;
          }
          
-         public String printExp(){
-             int countOpen = 0;
-             int countClose = 0;
-             
-             for(int i = 0; i < expressionString.length(); i++){
-                 if(expressionString.charAt(i) == '(' ){
-                     countOpen++;
-                 }else if(expressionString.charAt(i) == ')'){
-                     countClose++;
-                 }
-             
-             }
-             
-             for(int i = 0; i < countOpen - countClose; i++){
-                 expressionString += ")";
-             }
-             
-             return expressionString;
+         public double computeExp(boolean closeBrack){             
+               int stageCase = this.stage.removeFirst();
+               
+               switch(stageCase){
+                   case 0: 
+                       expressionString += "sin(pi*";
+                       this.stage.add(0);
+                       return Math.sin(Math.PI*computeExp(true));
+                       
+                   case 1:
+                       expressionString += "cos(pi*";
+                       this.stage.add(1);
+                       return Math.cos(Math.PI*computeExp(true));
+                       
+                   case 2:
+                       expressionString += "avg(";
+                       this.stage.add(2);
+                       double first = computeExp(false);
+                       expressionString += ",";
+                       double second = computeExp(true);
+                       expressionString += ")";
+                       return avg(first, second);
+                   case 3: 
+                       this.stage.add(3);
+                       if(closeBrack == true){
+                           expressionString += "x)";                           
+                       }else{
+                           expressionString += "x";
+                       }
+                       
+                       return x;
+                       
+                   default:
+                       this.stage.add(4);
+                       if(closeBrack == true){
+                           expressionString += "y)";
+                       }else{
+                           expressionString += "y";
+                       }
+                       return y;
+                       
+               }
+         
          }
          
        
          
-         public static void main(String[] args){
-             Exp red = new Exp(12.0, 12.1);
-             System.out.println(red.evaluate(true));
-             System.out.println(red.printExp());
-             //System.out.println((int)Math.random()*2);
-             
+         public static void main(String[] args) throws Exception{
+            Exp redExp = new Exp();
+            redExp.setXY(12.3, 20.5);
+            System.out.println(redExp.genExp(true));
+            System.out.println(redExp.getExpString());
+            LinkedList dump = redExp.getStage();
+            System.out.println(dump);
+            //redExp.setXY(12.3, 20.5);
+            System.out.println(redExp.computeExp(true));
+             System.out.println(redExp.getExpString());
+            
          }
     }
         
