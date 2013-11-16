c95dc3e838ca0cfdd11124887ce324b0d129bfb0
ernggkiddie <erng.me@gmail.com>
2013-11-16 21:32:22 +0700

    create evaluate method, average method, and class main for test

--- RandomArt/src/randomart/Exp.java
+++ RandomArt/src/randomart/Exp.java
@@ -11,115 +11,101 @@
      * @author Chanidapa 55130500210
      */
     public class Exp {
-        private String temp;
-        private int currentIndex;
-        public Exp(){
-            this.temp = "";
-            this.currentIndex = 0;
-        }
         
-        public void expr(){
-            
-            for(int i = 0; i <= 8; i++){
-               
-                int value = 1;
-                
-                if(i < 2){
-                   value = (int) Math.round(Math.random()*2);
-                }
-                else{
-                    double a = Math.random()*5;
-                    value = (int)(a);
-                }
-
-                switch(value){
-                    case 0:
-                        this.temp += "sin(pi*";
-                        
-                        break;
-                    case 1:
-                        this.temp += "cos(pi*";
-                        
-                        break;
-                    case 2: 
-                        this.temp += "avg(";
-                        average();
-                        this.temp += ")";
-                        break;
-                    case 3: 
-                        this.temp += "x";
-                        countBracket();
-                        break;
-                    default: 
-                        this.temp += "y";
-                        countBracket();
-                        break;
-                }
-            }
-            System.out.println(this.temp);
-            System.out.println(this.temp.length());
-        }
+        private double x;
+        private double y;
+        private int count = 0;
+        private int level = 5;
+        private String expressionString = "";
         
-        //average method
-        public void average(){
-    
-            int count = 0;
-            count = (int)Math.round(Math.random()*5);
-            
-            switch(count){
-                case 0: this.temp += "xa ";
-                    break;
-                case 1: this.temp += "ya ";
-                    break;
-                case 2: this.temp += "sin(pi*a) ";
+         public Exp(double x, double y){
+             this.x = x;
+             this.y = y;
+             
+         }
+         
+         public double evaluate(boolean closeBrack){
+             int random;
+              if(this.count == 0){
+                  random = (int) (Math.random()*2);
+                  this.count++;
+              }else if(this.count < this.level){
+                  random = (int) (Math.random()*3);
+                  this.count++;
+              }else{
+                  random = (int) (Math.random()*4);
+                  this.count++;
+              }
+              
+               switch(random){
+                   case 0: 
+                       expressionString += "sin(pi*";
+                       return Math.sin(Math.PI*evaluate(true));
+                       
+                   case 1:
+                       expressionString += "cos(pi*";
+                       return Math.cos(Math.PI*evaluate(true));
+                       
+                   case 2:
+                       expressionString += "avg(";
    
-                    break;
-                case 3: this.temp += "cos(pi*a) ";
-                    
-                    break;
-                default: this.temp += "avg(a ";
-                    average();
-                    temp += ")";
-                    break;
-            }
-            this.temp += ",";
-            
-            int count2 = 0;
-            switch(count2){
-                case 0: this.temp += "xb ";
-                    break;
-                case 1: this.temp += "yb ";
-                    break;
-                case 2: this.temp += "sin(pi*b ";
-                    break;
-                default: this.temp += "cos(pi*b ";
-                    break;
-            }
-        }
-        
-        
-        public void countBracket(){
-            System.out.println("Current Index = " + this.currentIndex);
-            System.out.println("Content = " + this.temp);
-            System.out.println("Current length = " + this.temp.length());
-            int count = 0;
-            for(int i = this.currentIndex; i < this.temp.length(); i++){
-                if(this.temp.charAt(i) == '(' && this.temp.charAt(i-1) != 'g'){
-                    count += 1;
-                }
-                this.currentIndex = i;
-            }
-            for(int i = 0; i < count; i++){
-                this.temp += ")";
-            }
-            System.out.println(this.temp);
-            System.out.println("Current Index after count = " + this.currentIndex);
-            System.out.println("Count = " + count);
-            
-        }
-        public static void main(String[] args){
-            Exp temp = new Exp();
-            temp.expr();
+                       double first = evaluate(false);
+                       expressionString += ",";
+                       double second = evaluate(true);
+                       expressionString += ")";
+                       return avg(first, second);
+                   case 3: 
+                       if(closeBrack == true){
+                           expressionString += "x)";
+                       }else{
+                           expressionString += "x";
+                       }
+                       
+                       return x;
+                       
+                   default:
+                       if(closeBrack == true){
+                           expressionString += "y)";
+                       }else{
+                           expressionString += "y";
+                       }
+                       return y;
+                       
+               }
+         }
+         
+         public double avg(double x, double y){
+             return (x+y)/2;
+         }
+         
+         public String printExp(){
+             int countOpen = 0;
+             int countClose = 0;
+             
+             for(int i = 0; i < expressionString.length(); i++){
+                 if(expressionString.charAt(i) == '(' ){
+                     countOpen++;
+                 }else if(expressionString.charAt(i) == ')'){
+                     countClose++;
+                 }
+             
+             }
+             
+             for(int i = 0; i < countOpen - countClose; i++){
+                 expressionString += ")";
+             }
+             
+             return expressionString;
+         }
+         
        
-        }
+         
+         public static void main(String[] args){
+             Exp red = new Exp(12.0, 12.1);
+             System.out.println(red.evaluate(true));
+             System.out.println(red.printExp());
+             //System.out.println((int)Math.random()*2);
+             
+         }
     }
+        
