2ee4b2f25185f026e5483a1bcab33e83654a6453
ernggkiddie <erng.me@gmail.com>
2013-11-18 18:32:46 +0700

    create random class

---
+++ RandomArt/src/randomart_team01/Random.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package randomart_team01;
+
+/**
+ *
+ * @author erngg
+ */
+public abstract class Random {
+    
+    public Random(){
+    }
+    
+    public abstract double randomFunction(double x ,double y);
+  
+    public abstract String expression();
+    
+}
