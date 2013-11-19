ead2a5e3451fc816558dfcbbdce8280847a6df91
ernggkiddie <erng.me@gmail.com>
2013-11-19 15:13:29 +0700

    add image button

--- RandomArt/src/randomart_team01/FrameGUI.java
+++ RandomArt/src/randomart_team01/FrameGUI.java
@@ -10,6 +10,8 @@ import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 /**
@@ -18,34 +20,49 @@ import java.awt.*;
  * @version 
  */
 public class FrameGUI extends JFrame{
-    private GUI panel;
+    public GUI panel ;
     private JPanel down;
     private JPanel top;
     private JButton generateButton;
     private JButton functionButton;
     private JButton printString;
     
+    ImageIcon imageFun = new ImageIcon("Function.jpg");
+    ImageIcon imageGen = new ImageIcon("Generate.jpg");
+    
     public FrameGUI() {
-    setLayout(new BorderLayout());
-    panel = new GUI();
-    add(panel,BorderLayout.CENTER);
+        
+        setLayout(new BorderLayout());
+        panel = new GUI();
+        add(this.panel,BorderLayout.CENTER);
     
-    down = new JPanel();
-    down.setLayout(new FlowLayout());
-    down.add(generate());
-    down.add(function());
-    add(down,BorderLayout.SOUTH);
+        down = new JPanel();
+        down.setLayout(new FlowLayout());
+        down.add(generate());
+        down.add(function());
+        add(down,BorderLayout.SOUTH);
+        
     
-    top = new JPanel();
-    top.setLayout(new GridLayout(1,1));
-    top.add(printString());
-    add(top,BorderLayout.NORTH);
+//    top = new JPanel();
+//    top.setLayout(new GridLayout(1,1));
+//    top.add(printString());
+//    add(top,BorderLayout.NORTH);
     
 }
     public JButton generate(){
         generateButton = new JButton();
         generateButton.setSize(new Dimension(200,20));
         generateButton.setText("Generate");
+        generateButton.add(new JLabel(imageGen));
+        generateButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                GUI generate = new GUI();
+                generate.setShowPicture();
+                repaint();
+            }
+        });
         return generateButton;
     }
     
@@ -53,6 +70,20 @@ public class FrameGUI extends JFrame{
         functionButton = new JButton();
         functionButton.setSize(new Dimension(200,20));
         functionButton.setText("function");
+        functionButton.add(new JLabel(imageFun));
+        functionButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //FrameGUI temp = new FrameGUI();
+                Expression red = new Expression();
+                Expression green = new Expression();
+                Expression blue = new Expression();
+                System.out.println("red : " + red.expression());
+                System.out.println("green : " + green.expression());
+                System.out.println("blue : " + blue.expression());
+            }
+        });
         return functionButton;
     }
     
